3a0fb6a4033918b75247fa9c44187130f014d1cf
Rushiv Arora <rushivarora@Rushivs-iMac.local>
2026-03-06 01:08:45 -0500

    Added Formation Control Environments

---
+++ mpe2/simple_formation_v1.py
@@ -0,0 +1,3 @@
+from mpe2.simple_formation.simple_formation import env, parallel_env, raw_env
+
+__all__ = ["env", "parallel_env", "raw_env"]
---
+++ mpe2/simple_line_v1.py
@@ -0,0 +1,3 @@
+from mpe2.simple_line.simple_line import env, parallel_env, raw_env
+
+__all__ = ["env", "parallel_env", "raw_env"]
