9e1ab0b8e8945b0f65db0e3a04cb1004c9fc3101
Marek Wydmuch <marek@wydmuch.poznan.pl>
2026-04-05 09:14:39 +0200

    Add farama-notifications support (#44)

--- mpe2/__init__.py
+++ mpe2/__init__.py
@@ -1,1 +1,11 @@
-__version__ = "1.0.0"
+__version__ = "1.1.0"
+
+try:
+    import sys
+
+    from farama_notifications import notifications
+
+    if "minigrid" in notifications and __version__ in notifications["minigrid"]:
+        print(notifications["mpe2"][__version__], file=sys.stderr)
+except Exception:  # nosec
+    pass
